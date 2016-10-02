024dad4033c7fdc6720175cdbd5b19d4c9256eb5
venkatanish <anish996@gmail.com>
2016-10-02 18:08:18 +0530

    chnaged few pictures

---
+++ app/src/main/java/app/first/in/collegeprofiles/Tennis.java
@@ -0,0 +1,17 @@
+package app.first.in.collegeprofiles;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by venkatanish on 01/10/16.
+ */
+
+public class Tennis  extends AppCompatActivity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tennis_interest);
+    }
+}
