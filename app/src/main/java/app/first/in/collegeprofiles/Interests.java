5e604c3740d79b3e1a7265c950d9ba1bf7960d47
venkatesh <venkateshtata9@gmail.com>
2016-10-04 14:58:31 +0530

    Modified the smoothness of Interests Screen.

--- app/src/main/java/app/first/in/collegeprofiles/Interests.java
+++ app/src/main/java/app/first/in/collegeprofiles/Interests.java
@@ -1,34 +1,80 @@
 package app.first.in.collegeprofiles;
 
-import android.content.Intent;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * Created by venkateshtata on 02/10/16.
  */
 public class Interests extends AppCompatActivity {
 
-    Button btn_tennis;// have to include the other interest buttons.
+
+    ListView mobile_list;
+    ArrayList<Drawable> items ;
+    TextView interest;
+    Adapter adapter;
+
+
+
+
+
+
 
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.interests);
 
+        mobile_list = (ListView) findViewById(R.id.list_view);
+        items = new ArrayList<Drawable>() { };
+        items.add(getResources().getDrawable(R.drawable.coding));
+        items.add(getResources().getDrawable(R.drawable.dance));
+        items.add(getResources().getDrawable(R.drawable.coding));
+        items.add(getResources().getDrawable(R.drawable.dance));
+        items.add(getResources().getDrawable(R.drawable.coding));
+        items.add(getResources().getDrawable(R.drawable.dance));
+        items.add(getResources().getDrawable(R.drawable.coding));
+        items.add(getResources().getDrawable(R.drawable.dance));
 
-       btn_tennis = (Button)findViewById(R.id.btn_tennis);
 
 
-        btn_tennis.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Interests.this, Tennis.class);
-                startActivity(intent);
-            }
-        });
+
+        adapter = new Adapter(this,0,items);
+        mobile_list.setAdapter(adapter);
+
+    }
+}
+class Adapter extends ArrayAdapter<Drawable> {
+    ArrayList<Drawable> items;
+    ImageView image;
+    TextView interest;
+
+    public Adapter(Context c, int resources, ArrayList<Drawable>list){
+        super(c,resources,list);
+        this.items = list ;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_layout, parent, false);
+        }
+        image= (ImageView) convertView.findViewById(R.id.img);
+        image.setBackground(items.get(position));
+        return convertView;
+
     }
 
 }
