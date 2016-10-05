0aa286d4673fa603eb32de7a5443e1ca8343cac2
venkatesh <venkateshtata9@gmail.com>
2016-10-05 19:13:11 +0530

    Initialized all the Interest Cards with Switch statement, you can now directly add your screen in the "CASE"

--- app/src/main/java/app/first/in/collegeprofiles/Interests.java
+++ app/src/main/java/app/first/in/collegeprofiles/Interests.java
@@ -1,18 +1,23 @@
 package app.first.in.collegeprofiles;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+
+import static app.first.in.collegeprofiles.R.drawable.dance;
 
 /**
  * Created by venkateshtata on 02/10/16.
@@ -21,15 +26,11 @@ public class Interests extends AppCompatActivity {
 
 
     ListView mobile_list;
-    ArrayList<Drawable> items ;
+    ArrayList<Drawable> items;
     TextView interest;
     Adapter adapter;
 
-
-
-
-
-
+    ImageView coding;
 
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,32 +38,73 @@ public class Interests extends AppCompatActivity {
         setContentView(R.layout.interests);
 
         mobile_list = (ListView) findViewById(R.id.list_view);
-        items = new ArrayList<Drawable>() { };
-        items.add(getResources().getDrawable(R.drawable.coding));
-        items.add(getResources().getDrawable(R.drawable.dance));
-        items.add(getResources().getDrawable(R.drawable.coding));
-        items.add(getResources().getDrawable(R.drawable.dance));
-        items.add(getResources().getDrawable(R.drawable.coding));
-        items.add(getResources().getDrawable(R.drawable.dance));
-        items.add(getResources().getDrawable(R.drawable.coding));
-        items.add(getResources().getDrawable(R.drawable.dance));
+
+        mobile_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+               switch ((position)){
+                   case 0:
+                       Intent intent = new Intent(Interests.this, MainActivity.class);
+                       startActivity(intent);
+                       break;
 
 
 
+               }
 
-        adapter = new Adapter(this,0,items);
+
+               /* if(items.get(position)==getResources().getDrawable(R.drawable.coding)) {
+
+
+                    Intent intent = new Intent(Interests.this, MainActivity.class);
+                    startActivity(intent);
+                }*/
+
+                Log.d("TAG", "onItemClick: " + position);
+
+
+
+            }
+        });
+
+        items = new ArrayList<Drawable>() {
+        };
+        items.add(getResources().getDrawable(R.drawable.coding));
+        items.add(getResources().getDrawable(dance));
+        items.add(getResources().getDrawable(R.drawable.coding));
+        items.add(getResources().getDrawable(dance));
+        items.add(getResources().getDrawable(R.drawable.coding));
+        items.add(getResources().getDrawable(dance));
+        items.add(getResources().getDrawable(R.drawable.coding));
+        items.add(getResources().getDrawable(dance));
+
+
+        //coding = (ImageView)findViewById(R.drawable.coding);
+
+        //   coding.setOnClickListener(new View.OnClickListener() {
+        //     @Override
+        //  public void onClick(View v) {
+        //     Intent nxt = new Intent(Interests.this, MainActivity.class);
+        //      startActivity(nxt);
+        //}
+        //});
+
+
+        adapter = new Adapter(this, 0, items);
         mobile_list.setAdapter(adapter);
 
     }
 }
+
 class Adapter extends ArrayAdapter<Drawable> {
     ArrayList<Drawable> items;
     ImageView image;
     TextView interest;
 
-    public Adapter(Context c, int resources, ArrayList<Drawable>list){
-        super(c,resources,list);
-        this.items = list ;
+    public Adapter(Context c, int resources, ArrayList<Drawable> list) {
+        super(c, resources, list);
+        this.items = list;
     }
 
     @Override
@@ -71,7 +113,7 @@ class Adapter extends ArrayAdapter<Drawable> {
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_layout, parent, false);
         }
-        image= (ImageView) convertView.findViewById(R.id.img);
+        image = (ImageView) convertView.findViewById(R.id.img);
         image.setBackground(items.get(position));
         return convertView;
 
