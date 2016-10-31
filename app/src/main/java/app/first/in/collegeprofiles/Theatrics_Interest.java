074cfc32b84d7098af570ece28a9db361b4f317c
snigdharao28 <snigdha.snigz@gmail.com>
2016-10-31 18:47:47 +0530

    modified the buttons in login screen

--- app/src/main/java/app/first/in/collegeprofiles/Theatrics_Interest.java
+++ app/src/main/java/app/first/in/collegeprofiles/Theatrics_Interest.java
@@ -3,6 +3,9 @@ package app.first.in.collegeprofiles;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,7 +15,6 @@ import android.view.ViewGroup;
  */
 
 public class Theatrics_Interest  extends Fragment {
-
 
 
     @Nullable
@@ -27,5 +29,10 @@ public class Theatrics_Interest  extends Fragment {
 
         return rootView;
     }
+
+
+
+
+
 }
 
