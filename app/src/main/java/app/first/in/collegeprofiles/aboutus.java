7c079d5b8d76dc5fa05e3b770478e414c8c28146
venkatanish <anish996@gmail.com>
2016-11-11 16:18:36 +0530

    about us

---
+++ app/src/main/java/app/first/in/collegeprofiles/aboutus.java
@@ -0,0 +1,34 @@
+package app.first.in.collegeprofiles;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+/**
+ * Created by venkatanish on 10/11/16.
+ */
+
+
+
+
+public class aboutus extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about_us);
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                //Write your logic here
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
+
